4c5c0d71d0a2f192cbb70f4db0e5610815068a06
Mahdi <muhdmahdia36@gmail.com>
2024-01-24 17:03:42 +0100

    First commits

---
+++ client/src/App.tsx
@@ -0,0 +1,28 @@
+import "./App.scss";
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Components/Home/Home";
+import Search from "./Components/Search/Search";
+import Support from "./Components/Support/Support";
+import Info from "./Components/Info/Info";
+import Lounge from "./Components/Lounge/Lounge";
+import Travelers from "./Components/Travelers/Travelers";
+import Subscriber from "./Components/Subscribers/Subscribe";
+import Footer from "./Components/Footer/Footer";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <Search />
+      <Support />
+      <Info />
+      <Lounge />
+      <Travelers />
+      <Subscriber />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
