5b90533c2d9685a074f89d23b0e1fc55d59fbebf
Mahdi <muhdmahdia36@gmail.com>
2024-01-24 17:45:08 +0100

    Add inport from comp files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
 import "./App.scss";
-import Navbar from "./Components/Navbar/Navbar";
-import Home from "./Components/Home/Home";
-import Search from "./Components/Search/Search";
-import Support from "./Components/Support/Support";
-import Info from "./Components/Info/Info";
-import Lounge from "./Components/Lounge/Lounge";
-import Travelers from "./Components/Travelers/Travelers";
-import Subscriber from "./Components/Subscribers/Subscribe";
-import Footer from "./Components/Footer/Footer";
+import {
+  Footer,
+  Home,
+  Info,
+  Lounge,
+  Navbar,
+  Search,
+  Subscriber,
+  Support,
+  Travelers,
+} from "./Components";
 
 function App() {
   return (
