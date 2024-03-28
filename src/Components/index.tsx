8a69081f23c33a2e3c15c017b78f08bd37f761f6
mahdi <muhdmahdia36@gmail>
2024-03-28 20:52:25 +0100

    Convert project to use Vite

--- src/Components/index.tsx
+++
@@ -1,9 +0,0 @@
-export { default as Home } from "./Home/Home";
-export { default as Footer } from "./Footer/Footer";
-export { default as Info } from "./Info/Info";
-export { default as Lounge } from "./Lounge/Lounge";
-export { default as Navbar } from "./Navbar/Navbar";
-export { default as Search } from "./Search/Search";
-export { default as Subscriber } from "./Subscribers/Subscriber";
-export { default as Support } from "./Support/Support";
-export { default as Travelers } from "./Travelers/Travelers";
